c2b8c1670ca2fd1b942b51712dc7cf6fd0e81647
CharLemAznable <545541819@qq.com>
2019-12-19 13:50:33 +0800

    Add some metric, some refactor.

--- hook.go
+++ hook.go
@@ -1,167 +1,157 @@
 package main
 
 import (
-	"github.com/CharLemAznable/gokits"
-	"io/ioutil"
-	"net/http"
+    "github.com/CharLemAznable/gokits"
+    "io/ioutil"
+    "net/http"
 )
 
 type SonarPayload struct {
-	ServerUrl   string                  `json:"serverUrl"`
-	TaskId      string                  `json:"taskId"`
-	Status      string                  `json:"status"`
-	AnalysedAt  string                  `json:"analysedAt"`
-	ChangedAt   string                  `json:"changedAt"`
-	Project     SonarPayloadProject     `json:"project"`
-	Branch      SonarPayloadBranch      `json:"branch"`
-	QualityGate SonarPayloadQualityGate `json:"qualityGate"`
+    ServerUrl   string                  `json:"serverUrl"`
+    TaskId      string                  `json:"taskId"`
+    Status      string                  `json:"status"`
+    AnalysedAt  string                  `json:"analysedAt"`
+    ChangedAt   string                  `json:"changedAt"`
+    Project     SonarPayloadProject     `json:"project"`
+    Branch      SonarPayloadBranch      `json:"branch"`
+    QualityGate SonarPayloadQualityGate `json:"qualityGate"`
 }
 
 type SonarPayloadProject struct {
-	Key  string `json:"key"`
-	Name string `json:"name"`
-	Url  string `json:"url"`
+    Key  string `json:"key"`
+    Name string `json:"name"`
+    Url  string `json:"url"`
 }
 
 type SonarPayloadBranch struct {
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	IsMain bool   `json:"isMain"`
-	Url    string `json:"url"`
+    Name   string `json:"name"`
+    Type   string `json:"type"`
+    IsMain bool   `json:"isMain"`
+    Url    string `json:"url"`
 }
 
 type SonarPayloadQualityGate struct {
-	Name       string                             `json:"name"`
-	Status     string                             `json:"status"`
-	Conditions []SonarPayloadQualityGateCondition `json:"conditions"`
+    Name       string                             `json:"name"`
+    Status     string                             `json:"status"`
+    Conditions []SonarPayloadQualityGateCondition `json:"conditions"`
 }
 
 type SonarPayloadQualityGateCondition struct {
-	Metric           string `json:"metric"`
-	Operator         string `json:"operator"`
-	Value            string `json:"value"`
-	Status           string `json:"status"`
-	OnLeakPeriod     bool   `json:"onLeakPeriod"`
-	ErrorThreshold   string `json:"errorThreshold"`
-	WarningThreshold string `json:"warningThreshold"`
+    Metric           string `json:"metric"`
+    Operator         string `json:"operator"`
+    Value            string `json:"value"`
+    Status           string `json:"status"`
+    OnLeakPeriod     bool   `json:"onLeakPeriod"`
+    ErrorThreshold   string `json:"errorThreshold"`
+    WarningThreshold string `json:"warningThreshold"`
 }
 
 func qyhook(writer http.ResponseWriter, request *http.Request) {
-	projectKey := request.Header.Get(SonarProjectKeyHeaderName)
-	if 0 == len(projectKey) {
-		gokits.ResponseText(writer, "Request Illegal")
-		return
-	}
-	if !projectKeyRegexp.MatchString(projectKey) {
-		gokits.ResponseText(writer, "Ignored")
-		return
-	}
-	bytes, _ := ioutil.ReadAll(request.Body)
-	payload, ok := gokits.UnJson(string(bytes),
-		new(SonarPayload)).(*SonarPayload)
-	if !ok || nil == payload {
-		gokits.ResponseText(writer, "Request Illegal")
-		return
-	}
+    projectKey := request.Header.Get(SonarProjectKeyHeaderName)
+    if 0 == len(projectKey) {
+        gokits.ResponseText(writer, "Request Illegal")
+        return
+    }
+    if !projectKeyRegexp.MatchString(projectKey) {
+        gokits.ResponseText(writer, "Ignored")
+        return
+    }
+    bytes, _ := ioutil.ReadAll(request.Body)
+    payload, ok := gokits.UnJson(string(bytes),
+        new(SonarPayload)).(*SonarPayload)
+    if !ok || nil == payload {
+        gokits.ResponseText(writer, "Request Illegal")
+        return
+    }
 
-	go func() {
-		msg := buildMsg(projectKey, payload)
-		sendQyMessage(msg)
-	}()
+    go func() {
+        msg := buildMsg(projectKey, payload)
+        sendQyMessage(msg)
+    }()
 
-	gokits.ResponseText(writer, "OK")
+    gokits.ResponseText(writer, "OK")
 }
 
 func buildMsg(projectKey string, payload *SonarPayload) string {
-	msg := "项目`" + projectKey + "` `" +
-		payload.Branch.Name + "`分析: "
-	if "SUCCESS" == payload.Status {
-		msg += "完成"
-	} else {
-		msg += "未完成"
-	}
-	msg += "\n"
+    msg := "项目`" + projectKey + "` `" +
+        payload.Branch.Name + "`分析: "
+    if "SUCCESS" == payload.Status {
+        msg += "完成"
+    } else {
+        msg += "未完成"
+    }
+    msg += "\n"
 
-	msg += "> 质量阈: "
-	if "OK" == payload.QualityGate.Status {
-		msg += "<font color=\"info\">正常</font>"
-	} else {
-		msg += "<font color=\"warning\">错误</font>"
-	}
-	msg += "\n>\n"
+    msg += "> 质量阈: "
+    if "OK" == payload.QualityGate.Status {
+        msg += "<font color=\"info\">正常</font>"
+    } else {
+        msg += "<font color=\"warning\">错误</font>"
+    }
+    msg += "\n>\n"
 
-	for _, condition := range payload.QualityGate.Conditions {
-		if "new_coverage" == condition.Metric {
-			msg += "> 新代码覆盖率: "
-			if "NO_VALUE" == condition.Status {
-				msg += "<font color=\"comment\">-</font>"
-			} else if "OK" == condition.Status {
-				msg += "<font color=\"info\">" + condition.Value + "%</font>"
-			} else {
-				msg += "<font color=\"warning\">" + condition.Value + "%</font>"
-			}
-			msg += "\n"
+    qualityMap := map[string]string{}
+    for _, condition := range payload.QualityGate.Conditions {
+        appender, ok := SonarMetricAppenderMap[condition.Metric]
+        if !ok {
+            continue
+        }
+        title, ok := SonarMetricTitleMap[condition.Metric]
+        if !ok {
+            continue
+        }
+        qualityMap[condition.Metric] = appender(title, condition)
+    }
+    for _, metric := range SonarMetricNameArray {
+        quality, ok := qualityMap[metric]
+        if !ok {
+            continue
+        }
+        msg += quality
+    }
 
-		} else if "new_duplicated_lines_density" == condition.Metric {
-			msg += "> 新代码重复率: "
-			if "NO_VALUE" == condition.Status {
-				msg += "<font color=\"comment\">-</font>"
-			} else if "OK" == condition.Status {
-				msg += "<font color=\"info\">" + condition.Value + "%</font>"
-			} else {
-				msg += "<font color=\"warning\">" + condition.Value + "%</font>"
-			}
-			msg += "\n"
+    msg += "\n查看详情, 请点击[链接](" + payload.Branch.Url + ")"
+    return msg
+}
 
-		} else if "new_maintainability_rating" == condition.Metric {
-			msg += "> 新代码可维护率: "
-			if "NO_VALUE" == condition.Status {
-				msg += "<font color=\"comment\">-</font>"
-			} else if "OK" == condition.Status {
-				msg += "<font color=\"info\">" + parseRatingValueToGrade(condition.Value) + "</font>"
-			} else {
-				msg += "<font color=\"warning\">" + parseRatingValueToGrade(condition.Value) + "</font>"
-			}
-			msg += "\n"
+func ratingValueAppender(title string, condition SonarPayloadQualityGateCondition) string {
+    str := "> " + title
+    if "NO_VALUE" == condition.Status {
+        str += "<font color=\"comment\">-</font>"
+    } else if "OK" == condition.Status {
+        str += "<font color=\"info\">" + condition.Value + "%</font>"
+    } else {
+        str += "<font color=\"warning\">" + condition.Value + "%</font>"
+    }
+    str += "\n"
+    return str
+}
 
-		} else if "new_reliability_rating" == condition.Metric {
-			msg += "> 新代码可靠率: "
-			if "NO_VALUE" == condition.Status {
-				msg += "<font color=\"comment\">-</font>"
-			} else if "OK" == condition.Status {
-				msg += "<font color=\"info\">" + parseRatingValueToGrade(condition.Value) + "</font>"
-			} else {
-				msg += "<font color=\"warning\">" + parseRatingValueToGrade(condition.Value) + "</font>"
-			}
-			msg += "\n"
-
-		} else if "new_security_rating" == condition.Metric {
-			msg += "> 新代码安全率: "
-			if "NO_VALUE" == condition.Status {
-				msg += "<font color=\"comment\">-</font>"
-			} else if "OK" == condition.Status {
-				msg += "<font color=\"info\">" + parseRatingValueToGrade(condition.Value) + "</font>"
-			} else {
-				msg += "<font color=\"warning\">" + parseRatingValueToGrade(condition.Value) + "</font>"
-			}
-			msg += "\n"
-		}
-	}
-
-	msg += "\n查看详情, 请点击[链接](" + payload.Branch.Url + ")"
-	return msg
+func gradeAppender(title string, condition SonarPayloadQualityGateCondition) string {
+    str := "> " + title
+    if "NO_VALUE" == condition.Status {
+        str += "<font color=\"comment\">-</font>"
+    } else if "OK" == condition.Status {
+        str += "<font color=\"info\">" + parseRatingValueToGrade(condition.Value) + "</font>"
+    } else {
+        str += "<font color=\"warning\">" + parseRatingValueToGrade(condition.Value) + "</font>"
+    }
+    str += "\n"
+    return str
 }
 
 func parseRatingValueToGrade(value string) string {
-	if "1" == value {
-		return "A"
-	} else if "2" == value {
-		return "B"
-	} else if "3" == value {
-		return "C"
-	} else if "4" == value {
-		return "D"
-	} else {
-		return "?"
-	}
+    if "1" == value {
+        return "A"
+    } else if "2" == value {
+        return "B"
+    } else if "3" == value {
+        return "C"
+    } else if "4" == value {
+        return "D"
+    } else if "5" == value {
+        return "E"
+    } else {
+        return "?"
+    }
 }
--- val.go
+++ val.go
@@ -1,3 +1,48 @@
 package main
 
 const SonarProjectKeyHeaderName = "X-SonarQube-Project"
+
+type MetricAppender func(string, SonarPayloadQualityGateCondition) string
+
+var SonarMetricNameArray []string
+var SonarMetricTitleMap map[string]string
+var SonarMetricAppenderMap map[string]MetricAppender
+
+func init() {
+    SonarMetricNameArray = []string{
+        "reliability_rating",
+        "new_reliability_rating",
+        "security_rating",
+        "new_security_rating",
+        "sqale_rating",
+        "new_maintainability_rating",
+        "coverage",
+        "new_coverage",
+        "duplicated_lines_density",
+        "new_duplicated_lines_density",
+    }
+    SonarMetricTitleMap = map[string]string{
+        "reliability_rating":           "可靠性比率：　　",
+        "new_reliability_rating":       "新代码可靠率：　",
+        "security_rating":              "安全比率：　　　",
+        "new_security_rating":          "新代码安全率：　",
+        "sqale_rating":                 "可维护性：　　　",
+        "new_maintainability_rating":   "新代码可维护性：",
+        "coverage":                     "覆盖率：　　　　",
+        "new_coverage":                 "新代码覆盖率：　",
+        "duplicated_lines_density":     "重复行：　　　　",
+        "new_duplicated_lines_density": "新代码重复行：　",
+    }
+    SonarMetricAppenderMap = map[string]MetricAppender{
+        "reliability_rating":           gradeAppender,
+        "new_reliability_rating":       gradeAppender,
+        "security_rating":              gradeAppender,
+        "new_security_rating":          gradeAppender,
+        "sqale_rating":                 gradeAppender,
+        "new_maintainability_rating":   gradeAppender,
+        "coverage":                     ratingValueAppender,
+        "new_coverage":                 ratingValueAppender,
+        "duplicated_lines_density":     ratingValueAppender,
+        "new_duplicated_lines_density": ratingValueAppender,
+    }
+}
