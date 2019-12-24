68dffc0d6c3201d6797df9add8e14162d2a131de
CharLemAznable <545541819@qq.com>
2019-12-24 16:20:19 +0800

    Fixed release tag.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o sonar-qyhook.linux.bin
+go build -ldflags="-s -w" -o sonar-qyhook.linux.bin
 upx --brute sonar-qyhook.linux.bin
